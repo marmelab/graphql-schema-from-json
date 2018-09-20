f5cd80f963584b78143bf615cf0ab55839276eb3
marcel <roesler1@gmx.de>
2018-09-20 18:04:28 +0200

    export printSchema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import isRelationshipFieldImport from './isRelationshipField';
 import getSchemaFromDataImport from './getSchemaFromData';
+import * as graphql from 'graphql';
 
 export * from './nameConverter';
 export const isRelationshipField = isRelationshipFieldImport;
 export default getSchemaFromDataImport;
 export const getSchemaFromData = getSchemaFromDataImport;
+export const printSchema = graphql.printSchema;
